7fb5dce3ba8760c318372b8c70bed92e61be3aaa
KPoarch <69836163+KPoarch@users.noreply.github.com>
2021-09-28 01:51:38 -0500

    crap still doesn't work ughhhhhhhhhhhhhhhhhhhhhhhh

--- rtchatty/ClientApp/src/App.js
+++ rtchatty/ClientApp/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
 import { SideNav } from "./components/SideNav";
 
 // Export default means that you don't need to use braces around the class name
 // Source: https://stackoverflow.com/questions/47619405/why-does-my-react-component-export-not-work
-import Chat from './components/Chat';
-import Profile from './components/Profile';
+import Chat from "./components/Chat";
+import Profile from "./components/Profile";
 
-import './custom.css'
+import "./custom.css";
 
 export default class App extends Component {
   static displayName = App.name;
@@ -19,11 +19,11 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/chat' component={Chat} />
-        <Route path='/profile' component={Profile} /> 
+        <Route exact path="/" component={Home} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/fetch-data" component={FetchData} />
+        <Route path="/chat" component={Chat} />
+        <Route path="/profile" component={Profile} />
         <Route path="/side-nav" component={SideNav} />
       </Layout>
     );
--- rtchatty/ClientApp/src/components/SideNav.js
+++ rtchatty/ClientApp/src/components/SideNav.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Button, Col, Form, Input, Label, Row } from "reactstrap";
 import Container from "reactstrap/lib/Container";
 import ListGroup from "reactstrap/lib/ListGroup";
 import ListGroupItem from "reactstrap/lib/ListGroupItem";
 import Media from "reactstrap/lib/Media";
 import defaultProfilePic from "../Assets/Images/defaultProfilePic.png";
-
-// var users = [];
+import axios from "axios";
 
 var sideProfilePicStyle = {
   width: "64px",
@@ -16,80 +15,139 @@ var sideProfilePicStyle = {
   backgroundColor: "#70a7ff",
 };
 
-var sideNavStyle = {
-  //   width: "25%",
-  //   height: "100%",
-  //   top: "0",
-  //   left: "0",
-  backgroundColor: "#70a7ff",
-  transition: ".5s ease",
-  display: "flex",
-  flexDirection: "column",
-  float: "left",
-};
+// var sideNavStyle = {
+//   //   width: "25%",
+//   //   height: "100%",
+//   //   top: "0",
+//   //   left: "0",
+//   backgroundColor: "#70a7ff",
+//   transition: ".5s ease",
+//   display: "flex",
+//   flexDirection: "column",
+//   float: "left",
+// };
 
 var listGroupStyle = {
   backgroundColor: "#70a7ff",
 };
 
-export class SideNav extends Component {
-  static displayName = SideNav.name;
+const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RBZG1pbkB0ZXN0LmNvbSIsIm5iZiI6MTYzMjgxMTY5NCwiZXhwIjoxNjMyODE1Mjk0LCJpYXQiOjE2MzI4MTE2OTR9.DN9OchwCXz-XFbekktFnO6bFpVZaLumEpzJR6PcqagQ`;
 
+export class SideNav extends Component {
+  // static displayName = SideNav.name;
   constructor(props) {
     super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      collapsed: true,
+      error: null,
+      isLoaded: false,
+      users: [],
     };
-    this.getFriends();
   }
 
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
+  componentDidMount() {
+    axios
+      .get("https://localhost:5001/api/User/getUsers", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            users: result.items.toList(),
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error,
+          });
+        }
+      );
   }
+
+  // async getUsers() {
+  //   // post request to update user info
+  //   // i hardcoded some information to test but it works lol
+  //   // just need this component to have user information (from props im assuming)
+  //   // so that we can pass that information into the api request
+  //   await axios
+  //     .get("https://localhost:5001/api/user/getUsers", {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     })
+  //     .then(function (res) {
+  //       console.log(res);
+  //       users = res.data;
+  //       loading = false;
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //       errors = true;
+  //       loading = false;
+  //     });
+  //   console.log("Edited Value -> ");
+  // }
 
   render() {
-    return (
-      <Container className="h-100 w-25" style={sideNavStyle}>
-        {/* <Container className="float-left w-25 h-100 bg-darken-3"> */}
-        <ListGroup style={listGroupStyle}>
-          <ListGroupItem style={listGroupStyle}>
-            <Media middle left>
-              <Media
-                className="m-1"
-                src={defaultProfilePic}
-                style={sideProfilePicStyle}
-              />
-              <span>Hello World</span>
-            </Media>
-          </ListGroupItem>
-          <ListGroupItem style={listGroupStyle}>
-            <Media middle left>
-              <Media
-                className="m-1"
-                src={defaultProfilePic}
-                style={sideProfilePicStyle}
-              />
-              Hello World
-            </Media>
-          </ListGroupItem>
-        </ListGroup>
-      </Container>
-    );
-  }
-  async getFriends() {
-    try {
-      const response = await fetch("api/user/getusers");
-      const data = await response.json();
-      this.setState({ users: data, loading: false });
-    } catch (e) {
-      console.log("Get Users Failed", e);
+    const { errors, isLoaded, users } = this.state;
+    if (errors) {
+      return (
+        <div class="min-vh-100 float-start bg-black">
+          <ListGroup style={listGroupStyle}>
+            <ListGroupItem style={listGroupStyle}>
+              {errors.map((error) => (
+                <span> {error} </span>
+              ))}
+            </ListGroupItem>
+          </ListGroup>
+        </div>
+      );
+    } else if (!isLoaded) {
+      return (
+        <div class="min-vh-100 float-start bg-black">
+          <ListGroup style={listGroupStyle}>
+            <ListGroupItem style={listGroupStyle}>Loading...</ListGroupItem>
+          </ListGroup>
+        </div>
+      );
+    } else {
+      return (
+        // <Container className="h-100 w-25" style={sideNavStyle}>
+        <div class="min-vh-100 float-start bg-black">
+          {/* <Container className="float-left w-25 h-100 bg-darken-3"> */}
+          <ListGroup style={listGroupStyle}>
+            <ListGroupItem style={listGroupStyle}>
+              <Media middle left>
+                <Media
+                  className="m-1"
+                  src={defaultProfilePic}
+                  style={sideProfilePicStyle}
+                />
+                <span>{users.length} Success</span>
+              </Media>
+            </ListGroupItem>
+            <ListGroupItem style={listGroupStyle}>
+              <Media middle left>
+                <Media
+                  className="m-1"
+                  src={defaultProfilePic}
+                  style={sideProfilePicStyle}
+                />
+                Hello World
+              </Media>
+            </ListGroupItem>
+          </ListGroup>
+          {/* </Container> */}
+        </div>
+      );
     }
-    // const response = await fetch("weatherforecast");
-    // const data = await response.json();
-    // this.setState({ forecasts: data, loading: false });
   }
 }
