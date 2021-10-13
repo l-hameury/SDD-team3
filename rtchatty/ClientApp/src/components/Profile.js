085098cd014905b817c7d1cf5e5a54687e1664c3
l-hameury <lhameury@uco.edu>
2021-10-13 11:53:35 -0500

    included a row in the profile, still have to get the status

--- rtchatty/ClientApp/src/components/Profile.js
+++ rtchatty/ClientApp/src/components/Profile.js
@@ -6,7 +6,8 @@ import {
   ListGroup, ListGroupItem, Modal,
   ModalBody, ModalHeader, ModalFooter,
   Container, Row, Col, Dropdown,
-  DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+  DropdownToggle, DropdownMenu, DropdownItem
+} from "reactstrap";
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import defaultProfilePic from "../Assets/Images/defaultProfilePic.png";
@@ -20,7 +21,7 @@ const Profile = () => {
     bio: "",
     username: ""
   })
-  const [currentInfo, setCurrentInfo] = useState({...userInfo})
+  const [currentInfo, setCurrentInfo] = useState({ ...userInfo })
   const [editInfoModal, setEditInfoModal] = useState(false);
   const [settingsDropdown, setSettingsDropdown] = useState(false);
   const [usernameError, setUsernameError] = useState("");
@@ -36,38 +37,38 @@ const Profile = () => {
         email: loggedInEmail
       }
     })
-    .then(function (res){
-      setUserInfo(res.data)
-      setCurrentInfo(res.data)
-    })
+      .then(function (res) {
+        setUserInfo(res.data)
+        setCurrentInfo(res.data)
+      })
   }, [])
 
   // to update userInfo state on input change
   const handleChange = (event) => {
     const key = event.target.name;
     const value = event.target.value;
-    if(key === 'username') validateUsername(value)
-    if(key === 'email') validateEmail(value);
-    if(!usernameError || !emailError) setUserInfo({ ...userInfo, [key]: value})
+    if (key === 'username') validateUsername(value)
+    if (key === 'email') validateEmail(value);
+    if (!usernameError || !emailError) setUserInfo({ ...userInfo, [key]: value })
   }
 
   // validation for username
   const validateUsername = (username) => {
-    if(username.trim() === "") setUsernameError('Username is required');
-    else if(username.length < 3) setUsernameError('Username must be at least 3 characters long');
+    if (username.trim() === "") setUsernameError('Username is required');
+    else if (username.length < 3) setUsernameError('Username must be at least 3 characters long');
     else setUsernameError('');
   }
-  
+
   // validation for email
   const validateEmail = (email) => {
-    if(email.length === 0) setEmailError('Email is required');
-    else if(!email.match("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+[.][a-zA-Z]{2,4}$")) setEmailError('Invalid Email Format')
+    if (email.length === 0) setEmailError('Email is required');
+    else if (!email.match("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+[.][a-zA-Z]{2,4}$")) setEmailError('Invalid Email Format')
     else setEmailError('');
   }
 
   // api request to update user information once submit button is pressed, if an error exists it will do nothing
   const updateInfo = () => {
-    if(usernameError || emailError) return;
+    if (usernameError || emailError) return;
 
     axios.post("https://localhost:5001/api/user/profileUpdate", {
       id: userInfo.id,
@@ -76,15 +77,15 @@ const Profile = () => {
       avatar: userInfo.avatar,
       bio: userInfo.bio
     })
-    .then(function (){
-      setEditInfoModal(!editInfoModal);
-      setCurrentInfo({...userInfo})
-      setSuccess("Profile successfully updated")
-    })
-    .catch(function (error){
-      if(error.response.data.includes("Username")) setUsernameError("Username is already taken.")
-      if(error.response.data.includes("Email")) setEmailError("Email is already taken.")
-    })
+      .then(function () {
+        setEditInfoModal(!editInfoModal);
+        setCurrentInfo({ ...userInfo })
+        setSuccess("Profile successfully updated")
+      })
+      .catch(function (error) {
+        if (error.response.data.includes("Username")) setUsernameError("Username is already taken.")
+        if (error.response.data.includes("Email")) setEmailError("Email is already taken.")
+      })
   }
 
   // to show the update modal for users to edit their information
@@ -92,13 +93,13 @@ const Profile = () => {
   const toggleSettingsDropdown = () => setSettingsDropdown(!settingsDropdown);
 
   // this resets information upon pressing the X button or the Close button in the Modal
-  const resetInfo = () =>{
-    setUserInfo({...currentInfo})
+  const resetInfo = () => {
+    setUserInfo({ ...currentInfo })
     setUsernameError("");
     setEmailError("");
     setSuccess("");
     setEditInfoModal(!editInfoModal)
-  } 
+  }
 
   return (
     <div>
@@ -106,27 +107,33 @@ const Profile = () => {
         <Card>
           <Row>
             <Col xs="3">
-              <CardImg 
-              className="rounded-circle" 
-              style={{padding: "5px"}}
-              onError={event => {event.target.src = defaultProfilePic;}} 
-              src={currentInfo.avatar ? currentInfo.avatar : defaultProfilePic}> 
+              <CardImg
+                className="rounded-circle"
+                style={{ padding: "5px" }}
+                onError={event => { event.target.src = defaultProfilePic; }}
+                src={currentInfo.avatar ? currentInfo.avatar : defaultProfilePic}>
               </CardImg>
             </Col>
             <Col xs="9">
               <Row>
                 <CardBody>
                   <ListGroup>
-                    <ListGroupItem style={{display: "inherit"}}>
+                    <ListGroupItem style={{ display: "inherit" }}>
                       <Col xs="2"> Username: </Col>
-                      <Col style={{textAlign: "right"}} className="profileInfo">
+                      <Col style={{ textAlign: "right" }} className="profileInfo">
                         <strong> <CardText id="userName" name="userName">{currentInfo.username}</CardText> </strong>
                       </Col>
                     </ListGroupItem>
-                    <ListGroupItem style={{display: "inherit"}}>
+                    <ListGroupItem style={{ display: "inherit" }}>
                       <Col xs="2"> Email: </Col>
-                      <Col style={{textAlign: "right"}}>
+                      <Col style={{ textAlign: "right" }}>
                         <strong> <CardText id="email" name="email">{currentInfo.email}</CardText> </strong>
+                      </Col>
+                    </ListGroupItem>
+                    <ListGroupItem style={{ display: "inherit" }}>
+                      <Col xs="2"> Status: </Col>
+                      <Col style={{ textAlign: "right" }}>
+                        <strong> <CardText id="email" name="email">{"online"}</CardText> </strong>
                       </Col>
                     </ListGroupItem>
                   </ListGroup>
@@ -138,17 +145,17 @@ const Profile = () => {
                 </CardBody>
               </Row>
             </Col>
-        </Row>
-        <CardFooter style={{textAlign: "right"}}>
-          <Dropdown isOpen={settingsDropdown} toggle={toggleSettingsDropdown} size="sm">
-            <DropdownToggle color="secondary">Settings</DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem onClick={toggleEditInfoModal}>Edit Profile</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </CardFooter>
-      </Card>
-      <Alert className="rounded" color="success" hidden={!success}>{success}</Alert>
+          </Row>
+          <CardFooter style={{ textAlign: "right" }}>
+            <Dropdown isOpen={settingsDropdown} toggle={toggleSettingsDropdown} size="sm">
+              <DropdownToggle color="secondary">Settings</DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem onClick={toggleEditInfoModal}>Edit Profile</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </CardFooter>
+        </Card>
+        <Alert className="rounded" color="success" hidden={!success}>{success}</Alert>
       </Container>
 
       <Modal isOpen={editInfoModal} toggle={resetInfo} className="updateModal">
@@ -167,12 +174,12 @@ const Profile = () => {
             </FormGroup>
             <FormGroup>
               <Label for="bio"> Bio:
-                <Input style={{width: "150%", height: "10rem"}} type="textarea" name={"bio"} id="bio" defaultValue={currentInfo.bio} onChange={handleChange}></Input>
+                <Input style={{ width: "150%", height: "10rem" }} type="textarea" name={"bio"} id="bio" defaultValue={currentInfo.bio} onChange={handleChange}></Input>
               </Label>
             </FormGroup>
             <FormGroup>
               <Label for="avatar"> Avatar Link:
-                <Input style={{width: "150%"}} name={"avatar"} id="avatar" defaultValue={currentInfo.avatar} onChange={handleChange}></Input>
+                <Input style={{ width: "150%" }} name={"avatar"} id="avatar" defaultValue={currentInfo.avatar} onChange={handleChange}></Input>
               </Label>
             </FormGroup>
           </Form>
@@ -184,7 +191,7 @@ const Profile = () => {
           <Button color="danger" onClick={resetInfo}>Cancel</Button>
         </ModalFooter>
       </Modal>
-     </div>
+    </div>
   );
 }
 
