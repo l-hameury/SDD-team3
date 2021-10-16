57c4ebf51bd564e53a994e2affbb07dbbf0ca7c0
KPoarch <69836163+KPoarch@users.noreply.github.com>
2021-10-16 09:20:50 -0500

    moved local storage call to inside use effect

--- rtchatty/ClientApp/src/components/UserNav.js
+++ rtchatty/ClientApp/src/components/UserNav.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ListGroup from "reactstrap/lib/ListGroup";
 import ListGroupItem from "reactstrap/lib/ListGroupItem";
-// import Row from "reactstrap/lib/Row";
 import Media from "reactstrap/lib/Media";
 import defaultProfilePic from "../Assets/Images/defaultProfilePic.png";
-// const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhvcGVAdGVzdC5jb20iLCJuYmYiOjE2MzMyMzk1OTEsImV4cCI6MTYzMzI0MzE5MSwiaWF0IjoxNjMzMjM5NTkxfQ.elJZK2BzyeliSXBvtA6DiarTQigkc9L5amr8mTMgdGg`;
-const token = localStorage.getItem("token").toString();
-// var displayUsers = [];
+
 var sideProfilePicStyle = {
   width: "64px",
   height: "64px",
@@ -32,6 +29,7 @@ export default function UserNav() {
   const [q, setQ] = useState("");
 
   useEffect(() => {
+    const token = localStorage.getItem("token").toString();
     // i wrapped the api request into a function
     // const search = async () => {
     fetch(`https://localhost:5001/api/User/searchUsers/`, {
