d42feb9c1d100fa55b151518c618cf5bd9de9872
Charlie <charliedecassios@gmail.com>
2021-10-27 19:23:35 -0500

    Story15 added to this branch and should be working

--- rtchatty/Controllers/UserController.cs
+++ rtchatty/Controllers/UserController.cs
@@ -40,7 +40,7 @@ namespace rtchatty.Controllers
         public ActionResult<User> GetPublicUserInfo(string username)
         {
             var user = service.GetPublicUserInfo(username);
-            return Ok(new { user.Username, user.Status, user.Bio, user.Avatar, user.CanMessage });
+            return Ok(new { user.Username, user.Status, user.Bio, user.Avatar, user.CanMessage, user.StatusShow });
         }
 
         [Route("isAdmin")]
@@ -136,14 +136,14 @@ namespace rtchatty.Controllers
             return Conflict(invalidItem);
         }
 
-        
+
         [Route("sendFriendRequest")]
         [HttpPost]
         public ActionResult<User> SendFriendRequest(User user)
         {
             return service.sendFriendRequest(user);
         }
-        
+
         [Route("confirmFriendRequest")]
         [HttpPost]
         public ActionResult<User> ConfirmFriendRequest(User user)
@@ -157,7 +157,7 @@ namespace rtchatty.Controllers
         {
             return service.deleteFriendRequest(user);
         }
-        
+
         [Route("ignoreRequest")]
         [HttpPost]
         public ActionResult<User> IgnoreRequest(User user)
