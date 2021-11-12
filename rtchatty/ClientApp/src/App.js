54a10d47b5fa0293022a372c7c696007ec1292e5
l-hameury <lhameury@uco.edu>
2021-11-12 01:12:25 -0600

    dark mode ok, did a workaround for the font issue

--- rtchatty/ClientApp/src/App.js
+++ rtchatty/ClientApp/src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
 import { FetchData } from "./components/FetchData";
 import { Counter } from "./components/Counter";
+import { light, dark } from "./components/Themes"
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/Style";
 // Export default means that you don't need to use braces around the class name
 // Source: https://stackoverflow.com/questions/47619405/why-does-my-react-component-export-not-work
 import Chat from "./components/Chat";
@@ -19,6 +22,10 @@ import LogOut from "./components/LogOut";
 
 // refactoring App.js
 function App() {
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
   //assign the results to a token variable
   const { token, setToken } = useToken();
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
@@ -26,29 +33,35 @@ function App() {
   // sending token to prop in Login
 
   // Return register page if the pathname matches and there is no token
-  if(useLocation().pathname === "/register" && !token){
+  if (useLocation().pathname === "/register" && !token) {
     // This is missing the Layout component, so it will not have the Nav bar.
     return <Route exact path="/register" component={Register} />
-  } 
+  }
   // Else return login page if no token.
-  else if (!token){
+  else if (!token) {
     return <Login setToken={setToken} />;
   }
 
   return (
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/counter" component={Counter} />
-        <Route exact path="/fetch-data" component={FetchData} />
-        <Route exact path="/chat" component={Chat} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/user-nav" component={UserNav} />
-        <Route exact path="/admin" component={AdminPage} />
-        <Route exact path="/logout" component={LogOut} />
-      </Switch>
-    </Layout>
+    <ThemeProvider theme={theme === 'light' ? light : dark}>
+      <>
+        <GlobalStyles />
+        <Layout>
+          <button onClick={themeToggler}>Switch Theme</button>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/counter" component={Counter} />
+            <Route exact path="/fetch-data" component={FetchData} />
+            <Route exact path="/chat" component={Chat} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/user-nav" component={UserNav} />
+            <Route exact path="/admin" component={AdminPage} />
+            <Route exact path="/logout" component={LogOut} />
+          </Switch>
+        </Layout>
+      </>
+    </ThemeProvider>
   );
 }
 
