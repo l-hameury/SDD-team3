b97d769ca0cd0343b79b2b9b4e013490000353e5
Kristofer Norwood <kristofernorwood@gmail.com>
2021-11-12 21:48:44 -0600

    Story 52 darkmode (#58)
    * dark mode ok, did a workaround for the font issue
    
    * corrected a routing issue causdd by merges lol
    
    Co-authored-by: l-hameury <lhameury@uco.edu>

---
+++ rtchatty/ClientApp/src/components/Themes.js
@@ -0,0 +1,13 @@
+export const light =
+{
+    body: '#FFFFFF',
+    text: '#808080',
+    background: '#363537',
+}
+
+export const dark =
+{
+    body: '#464444',
+    text: '#808080',
+    background: '#464444',
+}
