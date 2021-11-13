d1d83b8a04045ebc40d978e42e1456fd51d3d4e9
l-hameury <60524410+l-hameury@users.noreply.github.com>
2021-11-12 21:42:41 -0600

    dark mode ok, did a workaround for the font issue (#50)
    * dark mode ok, did a workaround for the font issue
    
    * corrected a routing issue causdd by merges lol
    
    Co-authored-by: Kris Norwood <kristofernorwood@gmail.com>

--- rtchatty/ClientApp/src/App.js
+++ rtchatty/ClientApp/src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
 import { FetchData } from "./components/FetchData";
 import { Counter } from "./components/Counter";
+import { light, dark } from "./components/Themes"
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/Style";
 // Export default means that you don't need to use braces around the class name
 // Source: https://stackoverflow.com/questions/47619405/why-does-my-react-component-export-not-work
 import Chat from "./components/Chat";
@@ -21,6 +24,10 @@ import ChannelManage from "./components/ChannelManage";
 
 // refactoring App.js
 function App() {
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
   //assign the results to a token variable
   const { token, setToken } = useToken();
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
@@ -28,31 +35,37 @@ function App() {
   // sending token to prop in Login
 
   // Return register page if the pathname matches and there is no token
-  if(useLocation().pathname === "/register" && !token){
+  if (useLocation().pathname === "/register" && !token) {
     // This is missing the Layout component, so it will not have the Nav bar.
     return <Route exact path="/register" component={Register} />
-  } 
+  }
   // Else return login page if no token.
-  else if (!token){
+  else if (!token) {
     return <Login setToken={setToken} />;
   }
 
   return (
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/counter" component={Counter} />
-        <Route exact path="/fetch-data" component={FetchData} />
-        <Route exact path="/chat/:channel?" component={Chat} />
-        <Route exact path="/channel/search" component={ChannelSearch} />
-        <Route exact path="/channel/manage" component={ChannelManage} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/user-nav" component={UserNav} />
-        <Route exact path="/admin" component={AdminPage} />
-        <Route exact path="/logout" component={LogOut} />
-      </Switch>
-    </Layout>
+    <ThemeProvider theme={theme === 'light' ? light : dark}>
+      <>
+        <GlobalStyles />
+        <Layout>
+          <button onClick={themeToggler}>Switch Theme</button>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/counter" component={Counter} />
+            <Route exact path="/fetch-data" component={FetchData} />
+            <Route exact path="/chat/:channel?" component={Chat} />
+            <Route exact path="/channel/search" component={ChannelSearch} />
+            <Route exact path="/channel/manage" component={ChannelManage} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/user-nav" component={UserNav} />
+            <Route exact path="/admin" component={AdminPage} />
+            <Route exact path="/logout" component={LogOut} />
+          </Switch>
+        </Layout>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ rtchatty/ClientApp/src/components/Style.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components"
+export const GlobalStyles = createGlobalStyle`
+body
+{
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    transition: 0.60s;
+}
+`
